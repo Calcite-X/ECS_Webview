5b2aab7646f086c7a33e1fda29c3688555d0ad83
Rohit Pandey <rohitpandeyofficial777@gmail.com>
2022-09-08 19:47:50 +0530

    Portfolio Screener and Comprehensive portfolio work

--- Dashboard/calcitex-project/src/pages/ComprehensivePortfolio.jsx
+++ Dashboard/calcitex-project/src/pages/ComprehensivePortfolio.jsx
@@ -3,7 +3,204 @@ import React from 'react'
 const ComprehensivePortfolio = () => {
     return (
         <div>
-            Comprehensive
+            <form action="">
+                <div className="Compre_container1">
+                    <p>Evaluation Date</p>
+                    <input type="date" className='dates1'/>-
+                    <input type="date" className='dates2'/>
+                    <select>
+                        <option value=""> SELECT USER(S) </option>
+                        <option value=""> Family Head </option>
+                        <option value=""> Client </option>
+                    </select>
+                </div>
+                <div className="Compre_container2">
+                    <p>Choose Sections</p>
+                    <div className="subCompre_cont">
+                        <p>Order</p>
+                        <div className="sect_items">
+                            <label htmlFor="sect_items1">
+                                <input type="checkbox"  id="sect_items1" /> Executive Summary
+                            </label>
+                            <select>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                            </select>
+                        </div>
+                        <div className="sect_items">
+                            <label htmlFor="sect_items2">
+                                <input type="checkbox"  id="sect_items2" /> Asset Analysis
+                            </label>
+                            <select>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                            </select>
+                        </div>
+                        <div className="sect_items">
+                            <label htmlFor="sect_items3">
+                                <input type="checkbox"  id="sect_items3" /> Holding Analysis
+                            </label>
+                            <select>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                            </select>
+                        </div>
+                        <div className="sect_items">
+                            <label htmlFor="sect_items4">
+                                <input type="checkbox"  id="sect_items4" /> Investment Snapshot
+                            </label>
+                            <select>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                            </select>
+                        </div>
+                        <div className="sect_items">
+                            <label htmlFor="sect_items5">
+                                <input type="checkbox"  id="sect_items5" /> Portfolio Summary
+                            </label>
+                            <select>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                            </select>
+                        </div>
+                        <div className="sect_items">
+                            <label htmlFor="sect_items6">
+                                <input type="checkbox"  id="sect_items6" /> Unrealized Capital Gain
+                            </label>
+                            <select>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                            </select>
+                        </div>
+                        <div className="sect_items">
+                            <label htmlFor="sect_items7">
+                                <input type="checkbox"  id="sect_items7" /> Transaction Statement
+                            </label>
+                            <select>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                            </select>
+                        </div>
+                        <div className="sect_items">
+                            <label htmlFor="sect_items8">
+                                <input type="checkbox"  id="sect_items8" /> Folio Details
+                            </label>
+                            <select>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="Compre_container3">
+                    <p>Choose Product</p>  
+                    <label htmlFor="product_items1">
+                                <input type="checkbox"  id="product_items1" /> Mutual Fund
+                    </label>
+                    <label htmlFor="product_items2">
+                                <input type="checkbox"  id="product_items2" /> Shares & Bond
+                    </label>
+                    <label htmlFor="product_items3">
+                                <input type="checkbox"  id="product_items3" /> Fixed Deposit
+                    </label>
+                    <label htmlFor="product_items4">
+                                <input type="checkbox"  id="product_items4" /> Other Assets
+                    </label>
+                </div>
+                
+                
+                <div className="Compre_container4">
+                    <p>Display Option</p>
+                    <div className="subCompre_4">
+                        <label htmlFor="display_items1">
+                            <input type="checkbox"  id="display_items1" />Hide Folio No.
+                        </label>
+                    </div>
+                </div>
+
+
+                <div className="Compre_container5">
+                    <p>Data Option</p>
+                    <div className="subCompre_4">
+                        <label htmlFor="data_items1">
+                            <input type="checkbox"  id="data_items1" />Benchmark Return
+                        </label>
+                    </div>
+
+                    <div className="subCompre_41">
+                        <p>No. of holdings</p>
+                        <select>
+                            <option value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="50">50</option>
+                            <option value="All">All</option>
+                        </select>
+                    </div>
+                </div>
+
+
+                <div className="Compre_container6">
+                    <button>Download Report</button>
+                    <button>Send On Email</button>
+                    <button>Send on Whatsapp</button>
+                    <div>
+                        <label htmlFor="btn_items1">
+                            <input type="checkbox"  id="btn_items1" />CC with RM
+                        </label>
+                    </div>
+                </div>
+
+            </form>
         </div>
     )
 }
--- Dashboard/calcitex-project/src/pages/PortfolioScreener.jsx
+++ Dashboard/calcitex-project/src/pages/PortfolioScreener.jsx
@@ -3,7 +3,81 @@ import React from 'react'
 const PortfolioScreener = () => {
     return (
         <div>
-            PortafolioScreener
+            Report Configuration 
+            <div className="portfolio_container">
+                <form action="">
+                  <p>Assets Class :</p>
+                  <label htmlFor="assetdata1">
+                    <input type="radio" name="asset_data1" id="assetdata1" />Mutual Funds
+                  </label>
+                  <label htmlFor="assetdata2">
+                    <input type="radio" name="asset_data1" id="assetdata2" />Share & Bond
+                  </label>
+                  <p>Report Type :</p>
+                  <label htmlFor="reportdata1">
+                    <input type="radio" name="report_data1" id="reportdata1" />Client wise
+                  </label>
+                  <label htmlFor="reportdata2">
+                    <input type="radio" name="report_data1" id="reportdata2" />Scrip wise
+                  </label>
+                  <p>Filters:</p>
+                  <div className="filter_container1">
+                    <span className="subcontainer1">
+                        Holding Days
+                        <select>
+                            <option value=">"> {">"} </option>
+                            <option value="<"> {"<"} </option>
+                            <option value="="> {"="}</option>
+                        </select>
+                    </span>
+                    <input type="number"  className='numcounts' />
+                    <span className="subcontainer2">
+                        <span>ARN</span>
+                        <select>
+                            <option value=">"> {">"} </option>
+                            <option value="<"> {"<"} </option>
+                        </select>
+                    </span>
+                  </div>
+
+
+                  
+                  {/* To be done */}
+                  <div className="filter_container2">
+                        <select>
+                            <option value=""> SELECT USER(S) </option>
+                            <option value="Relationship Manager"> Relationship Manager </option>
+                            <option value="Sub Broker"> Sub Broker </option>
+                            <option value="Family Head"> Family Head </option>
+                            <option value="Client"> Client </option>
+                        </select>
+                        <select>
+                            <option value=""> Select </option>
+                            <option value="Relationship Manager"> Relationship Manager </option>
+                            <option value="Sub Broker"> Sub Broker </option>
+                            <option value="Family Head"> Family Head </option>
+                            <option value="Client"> Client </option>
+                        </select>
+                        <select>
+                            <option value=""> All Selected </option>
+                        </select>
+                        <select>
+                            <option value=""> All Selected </option>
+                        </select>
+                  </div>
+                  <p>View Type:</p>
+                  <label htmlFor="viewdata1">
+                    <input type="radio" name="view_data1" id="viewdata1" />On Screen
+                  </label>
+                  <label htmlFor="viewdata2">
+                    <input type="radio" name="view_data1" id="viewdata2" />XLS
+                  </label>
+                  <div>
+                     <input type="submit" id="Applyportbtn" name="Apply"/>
+                  </div>
+
+                </form>
+            </div>
         </div>
     )
 }
